e737a1c5665712ba8af9776ec779aff232d4c44b
dcarruth <car12047@byui.edu>
2018-01-27 19:03:50 -0700

    s

--- web/Assign03/shoppingCart.php
+++ web/Assign03/shoppingCart.php
@@ -38,7 +38,7 @@ session_start();
 		<div>
 			<form method="post" target="_self" action="shoppingCart.php">
 			<div>
-				<img class="items img-rounded" src="Assign03/img0.png"/>
+				<img class="items img-rounded" src="img0.png"/>
 				<button class="btn-default btn-lg" type="submit" name="watch" value="0">Add to Cart</button>
 			</div>
 			<div>
@@ -62,7 +62,7 @@ session_start();
 				<button class="btn-default btn-lg" type="submit" name="watch" value="5">Add to Cart</button>
 			</div>
 			<div>
-				<img class="items img-rounded" src="img7.png"/>
+				<img class="items img-rounded" src="h/img7.png"/>
 				<button class="btn-default btn-lg" type="submit" name="watch" value="7">Add to Cart</button>
 			</div>
 			<div>
