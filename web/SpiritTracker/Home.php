e819fc31b63a87b0318691e1ad40b12d1cae306e
dcarruth <car12047@byui.edu>
2018-02-06 18:36:16 -0700

    Trying Heroku db connection again

--- web/SpiritTracker/Home.php
+++ web/SpiritTracker/Home.php
@@ -11,7 +11,7 @@ try {
 	else {
 		$dbopts = parse_url($dbUrl);
 
-		$dbHost = $dbopts["host"];
+		$dbHost = "127.0.0.1";
 		$dbPort = $dbopts["port"];
 		$dbUser = $dbopts["user"];
 		$dbPassword = $dbopts["pass"];
